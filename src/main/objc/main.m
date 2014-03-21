46854dfdd238cea52e9fd22debaa45fd48485009
hill1303 <hill.1303@gmail.com>
2014-03-21 03:02:56 -0400

    Added coordination ambiguity example with a VERY small resource file, AdjNounArcs, as well as some new scripts

--- src/main/objc/main.m
+++ src/main/objc/main.m
@@ -11,10 +11,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#define MAXSTRING_LENGTH 80
+#define MAXSTRING_LENGTH 100 
 #define MAXLINE_LENGTH 1024
 
-#define ARCS_PATH "/home/hill1303/MadlyAmbiguous/src/main/resources/AteSpaghettiArcs"
+#define PPA_ARCS_PATH "/home/hill1303/MadlyAmbiguous/src/main/resources/AteSpaghettiArcs"
+#define COORD_ARCS_PATH "/home/hill1303/MadlyAmbiguous/src/main/resources/AdjNounArcs"
 #define ENG_COMMON_PATH "/home/hill1303/MadlyAmbiguous/src/main/resources/CommonEnglish"
 #define PREP_TAG " with/"
 #define ATE_WITH_REGEX "^\\bate\\b\\s.+\\s\\bwith\\b/.+"
@@ -33,14 +34,15 @@
 +(NSString *)getInputFromUser;
 +(NSString *)findNoun:(NSString *)input usingFile:(NSString *)filePath;
 +(NSUInteger)getPPAFrequency:(NSString *)noun givenRegex:(NSString *)pattern usingFile:(NSString *)filePath;
-+(NSUInteger)lineFrequencyPPA:(NSString *)line;
++(double)conditionalFrequencyOfAdjNoun:(NSString *)adjective givenNoun:(NSString *)noun usingFile:(NSString *)filePath;
++(NSUInteger)lineFrequency:(NSString *)line;
++(NSString *)prepPhraseExample;
++(NSString *)coordinationExample;
 @end
-
 
 /*----------------------------------------------------------------------------------------------*/
 /* Function Implementations */
 /*----------------------------------------------------------------------------------------------*/
-
 @implementation MadHelper
 
 +(NSString *)getInputFromUser {
@@ -55,8 +57,41 @@
 	return [NSString stringWithUTF8String: inputFromUser];
 }
 
-+(NSString *)findNoun:(NSString *)input usingFile:(NSString *)filePath {
 
++(double)conditionalFrequencyOfAdjNoun:(NSString *)adjective givenNoun:(NSString *)noun usingFile:(NSString *)filePath {
+	
+
+
+	NSString *fileContents = [NSString stringWithContentsOfFile:filePath encoding:NSUTF8StringEncoding error:nil];
+	NSRange searchRange = NSMakeRange(0, [fileContents length]);
+	NSError *error = nil;
+	
+	NSString *nounPattern = [NSString stringWithFormat: @"^\\b%@\\b\\s.+$", noun];
+
+	NSRegularExpression *regexNoun = [NSRegularExpression regularExpressionWithPattern:nounPattern options:NSRegularExpressionAnchorsMatchLines error:&error];
+	NSArray *nounMatches = [regexNoun matchesInString:fileContents options:0 range:searchRange];
+
+	NSString *adjNounPattern = [NSString stringWithFormat: @"^\\b%@\\b\\s+\\b%@\\b/JJ.*$", noun, adjective];
+	NSRegularExpression *regexAdjNoun = [NSRegularExpression regularExpressionWithPattern:adjNounPattern options:NSRegularExpressionAnchorsMatchLines error:&error];
+	NSArray *adjNounMatches = [regexAdjNoun matchesInString:fileContents options:0 range:searchRange];
+
+	NSUInteger nounTotal = 0;
+	for (NSTextCheckingResult *match in nounMatches) {
+		NSString *matchText = [fileContents substringWithRange:[match range]];
+		nounTotal += [MadHelper lineFrequency:matchText];
+	}
+	NSUInteger adjNounTotal = 0;
+	for (NSTextCheckingResult *match in adjNounMatches) {
+		NSString *matchText = [fileContents substringWithRange:[match range]];
+		adjNounTotal += [MadHelper lineFrequency:matchText];
+	}
+
+	return (((double) adjNounTotal)/((double) nounTotal)); 
+
+
+}
++(NSString *)findNoun:(NSString *)input usingFile:(NSString *)filePath {
+	
 	NSArray *wordsAndEmptyStrings = [input componentsSeparatedByCharactersInSet:[NSCharacterSet whitespaceCharacterSet]];
 	NSArray *words = [wordsAndEmptyStrings filteredArrayUsingPredicate:[NSPredicate predicateWithFormat:@"length > 0"]];
 	
@@ -97,13 +132,74 @@
 	return ret;
 }
 
-+(NSUInteger)lineFrequencyPPA:(NSString *)line {
-	
++(NSUInteger)lineFrequency:(NSString *)line {
+
 	NSRange range = [line rangeOfString:@"\t" options:NSBackwardsSearch];
 	NSString *frequencyString = [line substringFromIndex:range.location];
 	return [frequencyString intValue];
 }
 
++ (NSString *)coordinationExample {
+	NSLog(@"Complete the following sentence by filling in words separated by spaces for each blank.");
+	NSLog(@"The (ADJ) (NOUN) and (NOUN) made such a mess!");
+
+	NSString *userInput = [MadHelper getInputFromUser];
+
+
+	NSArray *wordsAndEmptyStrings = [userInput componentsSeparatedByCharactersInSet:[NSCharacterSet whitespaceCharacterSet]];
+	NSArray *words = [wordsAndEmptyStrings filteredArrayUsingPredicate:[NSPredicate predicateWithFormat:@"length > 0"]];
+
+	// Split input into array, space delimited
+	
+	NSString *adjective = [[words objectAtIndex:0] lowercaseString];
+	NSString *leftNoun = [[words objectAtIndex:1] lowercaseString];
+	NSString *rightNoun = [[words objectAtIndex:2] lowercaseString];
+	
+	double leftNounFreq = [MadHelper conditionalFrequencyOfAdjNoun:adjective givenNoun:leftNoun usingFile:@COORD_ARCS_PATH];
+	double rightNounFreq = [MadHelper conditionalFrequencyOfAdjNoun:adjective givenNoun:rightNoun usingFile:@COORD_ARCS_PATH];
+	
+	NSString *output;
+
+	if ((rightNounFreq*10) >= leftNounFreq) {
+		output = [NSString stringWithFormat: @"The %@ %@ and the %@ %@ made a mess!", adjective, leftNoun, adjective, rightNoun];
+	}
+	else {
+		output = [NSString stringWithFormat: @"The %@ and the %@ %@ made a mess!", rightNoun, adjective, leftNoun];
+	}
+	return output;
+}
+
++(NSString *)prepPhraseExample {
+	
+	NSLog(@"Complete the following sentence...\n");
+	NSLog(@"I ate spaghetti with");
+	
+	NSString *userInput = [MadHelper getInputFromUser];
+	
+	//Find noun
+	NSString *noun = [MadHelper findNoun:userInput usingFile:@ENG_COMMON_PATH];
+
+	NSUInteger ateFrequency = [MadHelper getPPAFrequency:noun givenRegex:@ATE_WITH_REGEX usingFile:@PPA_ARCS_PATH];
+	NSUInteger spagFrequency = [MadHelper getPPAFrequency:noun givenRegex:@SPAG_WITH_REGEX usingFile:@PPA_ARCS_PATH];
+	NSString *output;
+
+	if (ateFrequency > spagFrequency){
+		output = [NSString stringWithFormat: @"With %@ is how I ate the spaghetti, but of course the spaghetti dish didn't have %@ in it!", userInput, userInput];
+	}
+	else if (ateFrequency < spagFrequency){
+		output = [NSString stringWithFormat: @"The spaghetti with %@ is what I ate, which of course means the spaghetti dish had %@ in it!", userInput, noun];
+	}
+	else {
+		int coin = rand() % 2;
+		if (coin = HEADS){
+			output = [NSString stringWithFormat: @"With %@ is how I ate the spaghetti, but of course the spaghetti dish didn't have %@ in it!", userInput, userInput];
+		}
+		else {
+			output = [NSString stringWithFormat: @"The spaghetti with %@ is what I ate, which of course means the spaghetti dish had %@ in it!", userInput, noun];
+		}
+	}
+	return output;
+}
 
 +(NSUInteger)getPPAFrequency:(NSString *)noun givenRegex:(NSString *)pattern usingFile:(NSString *)filePath {
 
@@ -118,12 +214,10 @@
 	NSUInteger nounTotal = 0;
 	for (NSTextCheckingResult *match in nounMatches) {
 		NSString *matchText = [fileContents substringWithRange:[match range]];
-		nounTotal += [MadHelper lineFrequencyPPA:matchText];
-	
+		nounTotal += [MadHelper lineFrequency:matchText];
 	}
 	return nounTotal;
 }
-
 @end
 
 
@@ -136,39 +230,29 @@
 int main (int argc, const char *argv []) {
 
 	NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
-
 	NSLog(@"Welcome to the first MadlyAmbiguous demo!");
-	NSLog(@"Complete the following sentence...\n");
-	NSLog(@"I ate spaghetti with \b\b");
 	
-	NSString *userInput = [MadHelper getInputFromUser];
-	
-	//Find noun
-	NSString *noun = [MadHelper findNoun:userInput usingFile:@ENG_COMMON_PATH];
+	NSLog(@"Would you like to test it out?: Y/N)");
+	NSString *test = [MadHelper getInputFromUser];
 
-	NSUInteger ateFrequency = [MadHelper getPPAFrequency:noun givenRegex:@ATE_WITH_REGEX usingFile:@ARCS_PATH];
-	NSUInteger spagFrequency = [MadHelper getPPAFrequency:noun givenRegex:@SPAG_WITH_REGEX usingFile:@ARCS_PATH];
+	while ([test isEqualToString: @"Y"] ||[test isEqualToString: @"y"]) {
+		NSLog(@"Which example do you want to try?");
+		NSLog(@"Prepositional Phrase Attachment Ambiguity: Enter PPA");
+		NSLog(@"Coordination Ambiguity: Enter CA");
+		
+		NSString *ex = [MadHelper getInputFromUser];
 
-	if (ateFrequency > spagFrequency){
-		NSLog(@"With %@ is how I ate the spaghetti, but of course the spaghetti dish didn't have %@ in it!", userInput, userInput);
-		NSLog(@"Ate with Freq: %lu", ateFrequency);
-	}
-	else if (ateFrequency < spagFrequency){
-		NSLog(@"The spaghetti with %@ is what I ate, which of course means the spaghetti dish had %@ in it!", userInput, noun);
-		NSLog(@"Spaghetti with Freq: %lu", spagFrequency);
-	}
-	else {
-		NSLog(@"I don't really know... flipping a coin");
-		NSLog(@"Spaghetti with Freq: %lu", spagFrequency);
-		NSLog(@"Ate with Freq: %lu", ateFrequency);
-
-		int coin = rand() % 2;
-		if (coin = HEADS){
-			NSLog(@"With %@ is how I ate the spaghetti, but of course the spaghetti dish didn't have %@ in it!", userInput, userInput);
+		if ([ex isEqualToString: @"PPA"]){
+			NSString *output = [MadHelper prepPhraseExample];
+			NSLog(output);
 		}
-		else {
-			NSLog(@"The spaghetti with %@ is what I ate, which of course means the spaghetti dish had %@ in it!", userInput, noun);
+		else if ([ex isEqualToString: @"CA"]){
+			 NSString *output = [MadHelper coordinationExample];
+			 NSLog (output);
 		}
+
+		NSLog(@"Would you like to test it again?: Y/N)");
+		test = [MadHelper getInputFromUser];
 	}
 	[pool drain];
 	return 0;
